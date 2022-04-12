ce6f2d4722b9598b662e5393bce9b769bb744af2
iryssalvatico <iryssalvatico16@gmail.com>
2022-04-12 10:35:44 -0300

    aula11abril

---
+++ controller/Form.php
@@ -0,0 +1,15 @@
+
+<?php
+class Form
+{
+  private $message = "";
+  public function controller()
+  {
+    $form = new Template("view/form.html");
+    $this->message = $form->saida();
+  }
+  public function getMessage()
+  {
+    return $this->message;
+  }
+}
