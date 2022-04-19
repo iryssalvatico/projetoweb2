dfbaf18fbf48375380f4756b698f77e45d60093a
iryssalvatico <iryssalvatico16@gmail.com>
2022-04-19 10:52:11 -0300

    aula 19

--- controller/Form.php
+++ controller/Form.php
@@ -1,15 +1,36 @@
-
 <?php
 class Form
 {
   private $message = "";
+  public function __construct()
+  {
+    Transaction::open();
+  }
   public function controller()
   {
     $form = new Template("view/form.html");
     $this->message = $form->saida();
   }
+  public function salvar()
+  {
+    if (isset($_POST['exercício']) && isset($_POST['tempo']) && isset($_POST['descanso']))
+      try {
+        $conexao = Transaction::get();
+        $academia = new Crud('academia');
+        $exercicio = $conexao->quote($_POST['exercicio']);
+        $tempo = $conexao->quote($_POST['tempo']);
+        $descanso = $conexao->quote($_POST['descanso']);
+        $resultado = $academia->insert("exercicio,tempo,descanso", "$exercicio,$tempo,$descanso");
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+  }
   public function getMessage()
   {
     return $this->message;
+  }
+  public function __destruct()
+  {
+    Transaction::close();
   }
 }
--- index.php
+++ index.php
@@ -16,7 +16,7 @@ spl_autoload_register('classLoader');
 // Front Controller
 class Aplicacao
 {
-  private static $app = "/projetoewb2irys";
+  private static $app = "/irysweb2";
   public static function run()
   {
     $layout = new Template('view/layout.html');
